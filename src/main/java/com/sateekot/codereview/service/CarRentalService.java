17b919576bad61422e6decf1309ac70ef1fae1d7
skota1 <sateeshkumar.kota@infor.com>
2025-09-03 11:08:44 +0200

    Fixing review comments

--- src/main/java/com/sateekot/codereview/service/CarRentalService.java
+++ src/main/java/com/sateekot/codereview/service/CarRentalService.java
@@ -1,44 +1,36 @@
 package com.sateekot.codereview.service;
 
-import org.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.HashMap;
 public class CarRentalService {
 
-  private final Logger LOG = LoggerFactory.getLogger(CarRentalService.class);
-
-  private String carBrand;
+  private final Logger log = LoggerFactory.getLogger(getClass());
   private static final int total_no_of_cars = 10;
-  private static final int DEFAULT_DURATION = 30;
-  private int cars_remaining;
+  private static final int DEFAULT_DURATION_MIN = 30;
   private static final int total_no_of_bikes = 20;
+  private String carBrand;
+  private int cars_remaining;
+
   public void rentCar() {
-
     cars_remaining--;
-    LOG.info("Remaing cars available="+cars_remaining);
-    LOG.info("Car rented");
-
+    log.info("Remaing cars available="+cars_remaining);
+    log.info("Car rented");
   }
 
   public void returnCar() {
-
     cars_remaining++;
-    LOG.info("Remaing cars available="+cars_remaining);
-    LOG.debug("Car returned");
-
+    log.info("Remaing cars available="+cars_remaining);
+    log.debug("Car returned");
   }
 
   public boolean checkAvailability() {
     try {
       return cars_remaining != 0;
-    } catch (ArrayIndexOutOfBoundsException ioe) {
-      LOG.error("Exception occured");
     } catch (Exception ex) {
-      LOG.error("Exception occured");
+      log.error("Exception occured");
     }
     return true;
   }
-
 
 }
