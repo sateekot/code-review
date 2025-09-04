b14c94a2102fd5e01ed41e95388cf9f15a955634
skota1 <sateeshkumar.kota@infor.com>
2025-09-04 21:57:24 +0200

    Revert "Applied automated LLM fixes"
    This reverts commit 1f83b881eaa845c7f9e8fe2b45c1b436be11e36a.

--- src/main/java/com/sateekot/codereview/service/CodeReviewService.java
+++ src/main/java/com/sateekot/codereview/service/CodeReviewService.java
@@ -2,9 +2,7 @@ package com.sateekot.codereview.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
-@Service
 public class CodeReviewService {
   private final Logger logger = LoggerFactory.getLogger(getClass());
   private static final String REPOSITORY = "code-review";
@@ -16,7 +14,9 @@ public class CodeReviewService {
   }
 
   public String review(String repository) {
+
     logger.info("Reviewing code for repository: {}", repository);
     return "Reviewing code";
+
   }
 }
