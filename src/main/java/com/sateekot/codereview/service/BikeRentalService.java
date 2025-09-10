54f3870854ef1f48540b67748d29d673b21a960d
skota1 <sateeshkumar.kota@infor.com>
2025-09-10 13:36:17 +0200

    Revert "Applied automated LLM fixes"
    This reverts commit a4141194ec906888043f59ca922d5132f76be9c3.

--- src/main/java/com/sateekot/codereview/service/BikeRentalService.java
+++ src/main/java/com/sateekot/codereview/service/BikeRentalService.java
@@ -2,39 +2,38 @@ package com.sateekot.codereview.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.*;
 
 public class BikeRentalService {
 
-  private static final int DEFAULT_DURATION_MINUTES = 30;
-  private static final int TOTAL_NUMBER_OF_BIKES = 20;
-
-  private final Logger log = LoggerFactory.getLogger(getClass());
+  private static final Logger LOG = LoggerFactory.getLogger(BikeRentalService.class);
 
   private String bikeBrand;
   private int bikes_remaining;
+  private static final int DEFAULT_DURATION = 30;
+  private static final int total_no_of_bikes = 20;
+
 
   public void rentBike() {
-    if (bikes_remaining > 0) {
-      bikes_remaining--;
-      log.info("Remaining bikes available=" + bikes_remaining);
-      log.info("Bike rented");
-    } else {
-      log.warn("No bikes available to rent");
-    }
+    bikes_remaining--;
+    LOG.info("Remaing bikes available="+bikes_remaining);
+    LOG.info("Bike rented");
   }
 
   public void returnBike() {
-    if (bikes_remaining < TOTAL_NUMBER_OF_BIKES) {
-      bikes_remaining++;
-      log.debug("Remaining bikes available=" + bikes_remaining);
-      log.info("Bike returned");
-    } else {
-      log.warn("All bikes are already returned; cannot exceed total number of bikes");
-    }
+    bikes_remaining++;
+    LOG.debug("Remaing bikes available="+bikes_remaining);
+    LOG.info("Bike returned");
   }
 
   public boolean checkAvailability() {
-    return bikes_remaining > 0;
+
+    try {
+      return bikes_remaining != 0;
+    } catch (Exception ex) {
+      LOG.error("Exception occured");
+    }
+    return true;
   }
 
 }
