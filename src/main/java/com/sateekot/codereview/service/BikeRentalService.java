537c87a672e4fb7dd11e8538d5140dc1b12606bc
skota1 <sateeshkumar.kota@infor.com>
2025-09-16 09:02:46 +0200

    Applied automated LLM fixes

--- src/main/java/com/sateekot/codereview/service/BikeRentalService.java
+++ src/main/java/com/sateekot/codereview/service/BikeRentalService.java
@@ -2,38 +2,39 @@ package com.sateekot.codereview.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.*;
 
 public class BikeRentalService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(BikeRentalService.class);
+  private static final int DEFAULT_DURATION = 30;
+  private static final int TOTAL_NO_OF_BIKES = 20;
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
 
   private String bikeBrand;
-  private int bikes_remaining;
-  private static final int DEFAULT_DURATION = 30;
-  private static final int total_no_of_bikes = 20;
-
+  private int bikesRemaining;
 
   public void rentBike() {
-    bikes_remaining--;
-    LOG.info("Remaing bikes available="+bikes_remaining);
-    LOG.info("Bike rented");
+    if (bikesRemaining > 0) {
+      bikesRemaining--;
+      log.info("Remaining bikes available=" + bikesRemaining);
+      log.info("Bike rented");
+    } else {
+      log.warn("No bikes available to rent");
+    }
   }
 
   public void returnBike() {
-    bikes_remaining++;
-    LOG.debug("Remaing bikes available="+bikes_remaining);
-    LOG.info("Bike returned");
+    if (bikesRemaining < TOTAL_NO_OF_BIKES) {
+      bikesRemaining++;
+      log.debug("Remaining bikes available=" + bikesRemaining);
+      log.info("Bike returned");
+    } else {
+      log.warn("All bikes are already returned");
+    }
   }
 
   public boolean checkAvailability() {
-
-    try {
-      return bikes_remaining != 0;
-    } catch (Exception ex) {
-      LOG.error("Exception occured");
-    }
-    return true;
+    return bikesRemaining != 0;
   }
 
 }
